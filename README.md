3dcbe6b2689a78e933d9ed01a3d319218485bda3
Manuel Nunez <manuelnunezalmonte@gmail.com>
2023-03-17 19:12:11 -0500

    using encapsulation to avoid error-prone

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Book Practice 
-
+## Introduction to express
 * [x] introduction to express
 * [x] getting satrted with Node
-* [x] Saving time with Express
+
+## Saving time with Express
+* [x] Adding express middlewares
 * [x] Adding handlebars
 * [x] using handlebars views and layout template
 * [x] adding public folder for static files
 * [x] adding dynamic and static content
+
+## Version Control
+* [x] Implementing encapulation to avoid error-prone
--- meadowlark.js
+++ meadowlark.js
@@ -2,14 +2,8 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
+const { getFortune } = require('./lib/fortune')
 
-const fortunes = [
-  "Conquer your fears or they will conquer you.",
-  "Rivers need springs.", 
-  "Do not fear what you don't know.", 
-  "You will have a pleasant surprise.", 
-  "Whenever possible, keep it simple.",
-]
 
 // middleware
 app.use(express.static(__dirname + '/public'))
@@ -24,9 +18,10 @@ app.set('views', './views')
 app.get('/', (req, res) => res.render('home'))
 
 app.get('/about', (req, res) => {
-  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
-  res.render('about', {fortune: randomFortune})
-})
+  res.render('about', { fortune: getFortune() })
+}
+)
+app.get('/contact', (req, res)=> res.render('contact'))
 
 app.use((req, res) => {
   res.type('text/plain')
